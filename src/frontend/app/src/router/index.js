ad2620a5f30c2797eebcd21434f00a41af264def
ClementTvs <clementtravers010@gmail.com>
2026-03-09 12:20:56 +0100

    fix(frontend): import login page

--- src/frontend/app/src/router/index.js
+++ src/frontend/app/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from "../view/Home.vue"
 import Profile from "../view/Profile.vue"
 import Post from "../view/Post.vue"
+import Login from "../view/Login.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
