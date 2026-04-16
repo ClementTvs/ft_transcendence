7b6d9813922bc5a0adcc139ad82468fcd624c4a8
Arthur <arthur.perez66000@gmail.com>
2026-04-16 10:34:36 +0200

    feat front: align separation line

--- src/frontend/app/src/view/Settings.vue
+++ src/frontend/app/src/view/Settings.vue
@@ -223,7 +223,7 @@ const themes = [
     <aside :class="['fixed top-0 left-0 h-screen w-64 border-r flex flex-col z-10 transition-colors duration-300', themeClasses.sidebar]">
 
       <!-- Logo -->
-      <div :class="['px-6 py-6 border-b', isDark ? 'border-gray-700' : 'border-rose-100']"> <!--bar haut param -->
+      <div :class="['px-6 py-6 h-[72px] border-b', isDark ? 'border-gray-700' : 'border-rose-100']"> <!--bar haut param -->
         <div class="flex items-center gap-3">
           <div :class="['w-8 h-8 rounded-xl bg-gradient-to-br flex items-center justify-center shadow-sm', isDark ? 'bg-gradient-to-br from-gray-600 to-gray-900 shadow-gray-500' : 'bg-gradient-to-br from-rose-400 to-rose-600 shadow-rose-200']">
             <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" stroke-width="2.5" viewBox="0 0 24 24">
@@ -308,7 +308,7 @@ const themes = [
     <main class="ml-64 min-h-screen">
 
       <!-- Header -->
-      <div :class="['sticky top-0 backdrop-blur-sm border-b px-8 py-4 z-10 transition-colors duration-300', themeClasses.header]"> <!-- bar haut droite-->
+      <div :class="['sticky top-0 backdrop-blur-sm border-b px-8 py-4 h-[72px] z-10 transition-colors duration-300', themeClasses.header]"> <!-- bar haut droite-->
         <div class="flex items-center justify-between">
           <div>
             <h1 :class="['text-lg font-semibold', isDark ? 'text-gray-100' : 'text-gray-900']">{{ currentSection.title }}</h1>
